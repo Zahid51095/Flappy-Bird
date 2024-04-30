e643636b914adbee7e3665343db81d3e7bbe46b3
Zahid Hasan <zahidhasan140338@gmail.com>
2024-04-30 22:54:09 +0600

    bird has been shown

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import styled from "styled-components";
 const WALL_HEIGHT = 600;
 const WALL_WIDTH = 400;
+const BIRD_HEIGHT = 28;
+const BIRD_WIDTH = 33;
 
 const App = () => {
-  return <Home>
-    <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
-      <Bird/>
-    </Background>
-  </Home>;
+  return (
+    <Home>
+      <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
+        <Bird height={BIRD_HEIGHT} width={BIRD_WIDTH} />
+      </Background>
+    </Home>
+  );
 };
 
 const Home = styled.div`
@@ -20,12 +24,17 @@ const Home = styled.div`
 const Background = styled.div`
   background-image: url("./images/background-day.png");
   background-repeat: no-repeat;
-  background-size: ${(props) => props.width}px ${(props)=> props.height}px;
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
-
-
+  border: 2px solid black;
 `;
-const Bird = styled.div``;
+const Bird = styled.div`
+  position: absolute;
+  background-image: url("./images/yellowbird-upflap.png");
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+`;
 
 export default App;
