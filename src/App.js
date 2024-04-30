15c5e892e95f5cc3732605df6fa005b38125f9a7
Zahid Hasan <zahidhasan140338@gmail.com>
2024-04-30 16:26:10 +0600

    background image has been set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+const WALL_HEIGHT = 600;
+const WALL_WIDTH = 400;
+
+const App = () => {
+  return <Home>
+    <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
+      <Bird/>
+    </Background>
+  </Home>;
+};
+
+const Home = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Background = styled.div`
+  background-image: url("./images/background-day.png");
+  background-repeat: no-repeat;
+  background-size: ${(props) => props.width}px ${(props)=> props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
 
 
-function App() {
-  return (
-    <div className="App">
-        App
-    </div>
-  );
-}
+`;
+const Bird = styled.div``;
 
 export default App;
